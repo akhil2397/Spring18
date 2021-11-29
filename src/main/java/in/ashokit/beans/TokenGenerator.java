28b2ddd30d2197cf0c969a2e2e704bd05f412836
Akhil Thammishetty <akhilthamishetti@gmail.com>
2021-11-29 12:26:44 +0530

    changes

--- src/main/java/in/ashokit/beans/TokenGenerator.java
+++ src/main/java/in/ashokit/beans/TokenGenerator.java
@@ -3,7 +3,7 @@ package in.ashokit.beans;
 public class TokenGenerator {
 	
 	// to understand when object is created define a constructor
-	public TokenGenerator() {
+	  public TokenGenerator() {
 		System.out.println("TokenGeneraotor : Constructor");
 	}
 
