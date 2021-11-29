3fc8a479bb98c5918eadbad04ca1806baacf697a
Akhil Thammishetty <akhilthamishetti@gmail.com>
2021-11-29 12:35:13 +0530

    changes done in master

--- src/main/java/in/ashokit/beans/TokenGenerator.java
+++ src/main/java/in/ashokit/beans/TokenGenerator.java
@@ -1,7 +1,9 @@
 package in.ashokit.beans;
 
 public class TokenGenerator {
-	
+	 
+
+               // changes done in master branch
 	// to understand when object is created define a constructor
 	  public TokenGenerator() {
 		System.out.println("TokenGeneraotor : Constructor");
